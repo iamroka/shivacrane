a89e21cfcad7b0131a5667e6891f9e9654888a1b
iamroka <rohank1294@gmail.com>
2026-01-31 16:48:22 +0530

    changed Map Code to modern code

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 // Initialize and add the map
-function initMap() {
-    // The location of hotel
-    const hotel = { lat: 15.296722510545417, lng:  73.9694485846559};
-    const center = { lat: 15.296722510545417, lng:  73.977};
-    // The map, centered at hotel
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 16,
-        center: hotel,
+// function initMap() {
+//     // The location of hotel
+//     const hotel = { lat: 15.296722510545417, lng:  73.9694485846559};
+//     const center = { lat: 15.296722510545417, lng:  73.977};
+//     // The map, centered at hotel
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         zoom: 16,
+//         center: hotel,
+//         mapId: 'DEMO_MAP_ID',
         
-    });
-    // The marker, positioned at hotel
-    const marker = new google.maps.Marker({
-        position: hotel,
-        map: map,
-    });
-}
+//     });
+//     // The marker, positioned at hotel
+//     google.maps.event.addListener(map, 'ready', function() {
+//     const marker = new google.maps.marker.AdvancedMarkerElement({
+//         position: hotel,
+//         map: map,
+//         title: "Shiva Crane",
+        
+//     });
+// });
+// }
+// initMap();
 
 window.onscroll = function () { scrollFunction() };
 
